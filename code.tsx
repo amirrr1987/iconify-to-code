2787a1e1d82f1b81d97478008730b8242fa7e4fd
Amir Maghami <amaghami@haco.dc>
2022-07-06 13:14:47 +0430

    use figma api

--- code.tsx
+++ code.tsx
@@ -1,6 +1,15 @@
-const { widget } = figma;
-const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG, Input } =
-  widget;
+// This widget will open an Iframe window with buttons to show a toast message and close the window.
+
+const { widget } = figma
+const {
+  useEffect,
+  useSyncedState,
+  usePropertyMenu,
+  AutoLayout,
+  Text,
+  SVG,
+  Input,
+} = widget;
 
 const copy = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M13.3333 6H7.33333C6.59695 6 6 6.59695 6 7.33333V13.3333C6 14.0697 6.59695 14.6667 7.33333 14.6667H13.3333C14.0697 14.6667 14.6667 14.0697 14.6667 13.3333V7.33333C14.6667 6.59695 14.0697 6 13.3333 6Z" stroke="#5685EE" stroke-linecap="round" stroke-linejoin="round"/>
@@ -12,9 +21,20 @@ const paste = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns
 <path d="M9.5 2H6.5C6.22386 2 6 2.22386 6 2.5V3.5C6 3.77614 6.22386 4 6.5 4H9.5C9.77614 4 10 3.77614 10 3.5V2.5C10 2.22386 9.77614 2 9.5 2Z" stroke="#5685EE" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
 
+
 function Widget() {
-  const [text, setText] = useSyncedState("text", "");
-  const iconCode = `<Icon icon="${text}" size="" />`;
+  useEffect(() => {
+    figma.ui.onmessage = (msg) => {
+      if (msg.type === 'showToast') {
+        figma.notify('Hello widget')
+      }
+      if (msg.type === 'close') {
+        figma.closePlugin();
+      }
+    }
+  })
+    const [text, setText] = useSyncedState("text", "");
+    const iconCode = `<Icon icon="${text}" size="" />`;
 
   return (
     <AutoLayout
@@ -85,4 +105,4 @@ function Widget() {
   );
 }
 
-widget.register(Widget);
+widget.register(Widget)
